c738a71046782c2f735cd3039381633f1e17d810
AGolumbovsky <agolumbovsky@gmail.com>
2016-01-15 23:30:52 -0500

    started with encede() fn

--- public/app.js
+++ public/app.js
@@ -1,6 +1,24 @@
 console.log("Stierlitz ohuel");
 
 function encode() {
+
 	var gotText = document.getElementById('text-original').value;
-	console.log(gotText);
+
+	if(!gotText) alert("Your coded letter ain't much..");
+
+	else {
+
+		/*
+		- need to .split('') the text
+		- locate spaces and anchor words to them
+		- space-1 && space+1 dont' change
+		- everything between s-1 and s+1 is scrambled
+		- .join
+		- profit
+		*/
+
+		var gaveText = document.getElementById('text-encoded');
+		gaveText.value = "Wait for it...";
+
+	}
 }
