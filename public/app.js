f190c878dfb0c0b9b0b043995d1c0e72f420ff4e
AGolumbovsky <agolumbovsky@gmail.com>
2016-01-16 01:07:22 -0500

    shuffled inbetweens

--- public/app.js
+++ public/app.js
@@ -1,22 +1,40 @@
-console.log("Stierlitz ohuel");
+
+function shuffle (arr) {
+
+	var i = 0,
+		j = 0,
+		temp = null;
+	
+	for (i = arr.length-1; i>0; i-=1) {
+		j = Math.floor(Math.random() * (i + 1));
+		temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	console.log(arr + " shuffled");
+	return arr;
+}
 
 function encode() {
 
 	var gotText = document.getElementById('text-original').value;
 
-	if(!gotText) alert("Your coded letter ain't much..");
+	if (gotText.length <= 3) alert("Your coded letter ain't much.."); //check more
 
 	else {
 
-		/*
-		- need to .split('space') the text and store in arr
-		- iterate over the arr
-			-check for words<=3
-			-scramble letters between word[0] and word[length-1]
-		- .join
-		- profit
-		*/
+		var words = gotText.split(' ');
 
+		for (var i=0; i<words.length; i++) {
+
+			var obrez = words[i].substring(1, words[i].length-1).split('');
+			var obrezM = shuffle(obrez);
+
+				
+			console.log(obrezM);
+
+		}
+		
 		var gaveText = document.getElementById('text-encoded');
 		gaveText.value = "Wait for it...";
 
