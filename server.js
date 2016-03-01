d30ba93371ade647578103cae75e0d753a0f7f11
AGolumbovsky <agolumbovsky@gmail.com>
2016-03-01 17:19:50 -0500

    selenium tests

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 
+// initialize app
 var app = express();
 
+// define port
 var port = 8777;
 
 // serve static files
 app.use(express.static(__dirname + '/public'));
 
+// listen to port
 app.listen(port, function() {
 	console.log("listening on port " + port);
 });
